4e45f1f649890de0b4cc6ff1d8ca7186c86ef359
jubinlathif <jubin.lathif@enett.com>
2019-01-21 14:42:45 +1100

    (Exporting users from AD)

---
+++ Export_AD_Group_Members.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module ActiveDirectory
+Get-ADGroupMember -identity “jira-users” | select name | Export-csv -path C:\Users\deon.grobler\Documents\Work\AD_Groups\Groupmembers.csv -NoTypeInformation
+Import-module
+Install-Module -Name MicrosoftTeams -RequiredVersion 0.9.0 
+get-team
